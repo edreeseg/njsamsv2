d950deedd5e3d90f991152e8298d682ac2882d47
edreeseg <edward.reeseg@gmail.com>
2019-11-10 21:49:52 -0500

    Some small changes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,24 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 export default App;
 
-const store = createStore(reducer);
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>NJSAMS</h1>
+      <p>Content</p>
     </div>
-  );
+  )
 }
---
+++ front-end/src/components/Search/SearchContainer.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function SearchContainer() {
+    return (
+        <section />
+    );
+}
+
+export default SearchContainer;
